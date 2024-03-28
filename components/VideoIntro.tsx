c13b0aefd0b3830327fdcdd149aa18c9ea04bab1
Aswin-art <aswinarung2@gmail.com>
2024-03-28 12:36:31 +0700

    feat: add sub intro video

--- components/VideoIntro.tsx
+++ components/VideoIntro.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const VideoIntro = () => {
   return (
     <video autoPlay playsInline className="w-full max-h-screen object-cover">
-      <source src={"/videos/intro.mp4"} type="video/mp4" />
+      <source src={"/videos/intro.mov"} type="video/mp4" />
       Maaf, browser Anda tidak mendukung video HTML5 dengan format yang
       diberikan.
     </video>
