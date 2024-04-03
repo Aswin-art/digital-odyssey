91298a1347b9d7324b08b8c344b15f8faa45c7e9
Aswin-art <aswinarung2@gmail.com>
2024-04-03 18:19:48 +0700

    feat: add modal resolution

--- app/page.tsx
+++ app/page.tsx
@@ -44,82 +44,92 @@ export default function Home() {
 
   return (
     <>
-      {!gameState.modalHeadphone ? (
-        <Modal
-          title="Tips Sebelum Bermain"
-          desc="Gunakan headphone untuk kenyamanan bermain"
-          btnText="Mengerti!"
-          onClickFunc={updateModalIntroState}
-        />
-      ) : (
-        <>
-          {!gameState.isPlay ? (
-            <>
-              {/* <Navbar /> */}
-              <main
-                className="flex flex-col items-center min-h-screen justify-end gap-5 p-24"
-                style={{
-                  backgroundImage: `url('/images/bg-hero.png')`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
-              >
-                <h1
-                  className={`text-4xl text-white font-bold ${myFont.className}`}
-                >
-                  digital odyssey
-                </h1>
-                <h1 className="text-6xl text-white font-bold">
-                  The Canonical Chronicles
-                </h1>
-                <h1
-                  className={`text-2xl text-black font-bold px-14 py-3 ${myFont.className}`}
-                  id="buttonPlay"
+      <div className="hidden lg:block">
+        {!gameState.modalHeadphone ? (
+          <Modal
+            title="Tips Sebelum Bermain"
+            desc="Gunakan headphone untuk kenyamanan bermain"
+            btnText="Mengerti!"
+            onClickFunc={updateModalIntroState}
+          />
+        ) : (
+          <>
+            {!gameState.isPlay ? (
+              <>
+                {/* <Navbar /> */}
+                <main
+                  className="flex flex-col items-center min-h-screen justify-end gap-5 p-24"
                   style={{
-                    backgroundImage: `url('/images/btn-background.webp')`,
+                    backgroundImage: `url('/images/bg-hero.png')`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
-                    transition: "filter 0.3s",
                   }}
-                  onMouseEnter={handleHover}
-                  onMouseLeave={handleHoverExit}
-                  onClick={handleButtonClick}
                 >
-                  play games
-                </h1>
-              </main>
-              <MusicBackground />
-            </>
-          ) : (
-            <>
-              {gameState.isIntroEnded ? (
-                <Modal
-                  title="Coming Soon"
-                  desc="Game masih dalam tahap pengembangan"
-                  btnText="kembali"
-                  onClickFunc={() => console.log("oke")}
-                />
-              ) : (
-                <VideoIntro />
-              )}
-            </>
-          )}
+                  <h1
+                    className={`text-4xl lg:none text-white font-bold ${myFont.className}`}
+                  >
+                    digital odyssey
+                  </h1>
+                  <h1 className="text-6xl text-white font-bold">
+                    The Canonical Chronicles
+                  </h1>
+                  <h1
+                    className={`text-2xl text-black font-bold px-14 py-3 ${myFont.className}`}
+                    id="buttonPlay"
+                    style={{
+                      backgroundImage: `url('/images/btn-background.webp')`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      transition: "filter 0.3s",
+                    }}
+                    onMouseEnter={handleHover}
+                    onMouseLeave={handleHoverExit}
+                    onClick={handleButtonClick}
+                  >
+                    play games
+                  </h1>
+                </main>
+                <MusicBackground />
+              </>
+            ) : (
+              <>
+                {gameState.isIntroEnded ? (
+                  <Modal
+                    title="Coming Soon"
+                    desc="Game masih dalam tahap pengembangan"
+                    btnText="kembali"
+                    onClickFunc={() => console.log("oke")}
+                  />
+                ) : (
+                  <VideoIntro />
+                )}
+              </>
+            )}
 
-          <AnimatedCursor
-            innerSize={10}
-            outerSize={45}
-            innerScale={1}
-            outerScale={2}
-            outerAlpha={0}
-            innerStyle={{
-              backgroundColor: "white",
-            }}
-            outerStyle={{
-              border: "3px solid white",
-            }}
-          />
-        </>
-      )}
+            <AnimatedCursor
+              innerSize={10}
+              outerSize={45}
+              innerScale={1}
+              outerScale={2}
+              outerAlpha={0}
+              innerStyle={{
+                backgroundColor: "white",
+              }}
+              outerStyle={{
+                border: "3px solid white",
+              }}
+            />
+          </>
+        )}
+      </div>
+      <div className="block lg:hidden">
+        <Modal
+          title="Resolusi Tidak Sesuai"
+          desc="Buka dan mainkan di resolusi minimal 1024px"
+          btnText="Mengerti!"
+          onClickFunc={() => null}
+        />
+      </div>
     </>
   );
 }
