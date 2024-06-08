60ef7fe97387a0e0622894e29584a56f23d280eb
Aswin-art <aswinarung2@gmail.com>
2024-06-09 03:47:51 +0700

    fix: disable user/api

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,18 @@
-import { GetCurrentUser } from "@/models/user";
+// import { GetCurrentUser } from "@/models/user";
+// import { NextRequest } from "next/server";
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     const user = GetCurrentUser();
+//     return Response.json({ message: "success", data: user });
+//   } catch (err) {
+//     console.log(err);
+//     return Response.json({ message: err, data: null });
+//   }
+// }
+
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  try {
-    const user = GetCurrentUser();
-    return Response.json({ message: "success", data: user });
-  } catch (err) {
-    console.log(err);
-    return Response.json({ message: err, data: null });
-  }
+  return Response.json({ message: "oke" });
 }
